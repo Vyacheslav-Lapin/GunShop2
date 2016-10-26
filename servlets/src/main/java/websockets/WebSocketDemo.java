3ae63524f16056d0a8d68a59226c5613939a1c7a
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-10-27 01:02:33 +0300

    WebSocket advanced example - with server pushing support

--- servlets/src/main/java/websockets/WebSocketDemo.java
+++ servlets/src/main/java/websockets/WebSocketDemo.java
@@ -1,13 +1,44 @@
 package websockets;
 
+import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/echo")
 public class WebSocketDemo {
 
+//    @OnMessage
+//    public String echoTextMessage(String msg) {
+//        return msg + " from WebSocket!";
+//    }
+
+    private static Set<Session> userSessions = Collections.newSetFromMap(
+            new ConcurrentHashMap<Session, Boolean>());
+
+    @OnOpen
+    public void onOpen(Session userSession) {
+        userSessions.add(userSession);
+        userSession.getAsyncRemote().sendText("Hi!");
+    }
+
+    @OnClose
+    public void onClose(Session userSession) {
+        userSessions.remove(userSession);
+    }
+
     @OnMessage
-    public String echoTextMessage(String msg) {
-        return msg + " from WebSocket!";
+    public void onMessage(String message) {
+        broadcast(message + " from WebSocket!");
+    }
+
+    public static void broadcast(String msg) {
+        userSessions.stream()
+                .map(Session::getAsyncRemote)
+                .forEach(async -> async.sendText(msg));
     }
 }
--- servlets/src/main/webapp/_scripts/MyWebSocket.js
+++ servlets/src/main/webapp/_scripts/MyWebSocket.js
@@ -4,32 +4,40 @@ const WS_URI = "ws://localhost:8080/echo";
 
 class MyWebSocket {
 
-    constructor(outputElementId = "output", inputElementId = "message", senderElementId = "textID") {
+    constructor(outputElementId = "output", inputElementId = "message") {
         /** @type HTMLDivElement */
         this.output = document.getElementById(outputElementId);
 
         /** @type HTMLInputElement */
         this.input = document.getElementById(inputElementId);
 
-        document.getElementById(senderElementId).addEventListener("click",
-            () => this.doSend(this.input.value));
+        this.input.form.addEventListener("submit", evt => {
+            this.doSend(this.input.value);
+            this.input.value = "";
+            evt.preventDefault();
+        }, true);
 
-        this.websocket = new WebSocket(WS_URI);
-        this.websocket.onopen = () => this.writeToScreen(`Connected to Endpoint!`);
-        this.websocket.onmessage = evt => this.writeToScreen(`Message Received: ${evt.data}`);
-        this.websocket.onerror = evt => this.writeToScreen(`ERROR: ${evt.data}`);
+        //noinspection JSUnusedGlobalSymbols,SpellCheckingInspection
+        this.webSocket = Object.assign(new WebSocket(WS_URI), {
+            onopen: () => this.writeToScreen(`Connected to Endpoint!`),
+            onmessage: evt => this.writeToScreen(`Message Received: ${evt.data}`),
+            onerror: evt => this.writeToScreen(`ERROR: ${evt.data}`)
+        });
 
-        addEventListener("beforeunload", this.websocket.close.bind(this.websocket));
+        //noinspection SpellCheckingInspection
+        addEventListener("beforeunload",
+            this.webSocket.close
+                .bind(this.webSocket));
     }
 
     writeToScreen(message) {
-        const pre = document.createElement("p");
-        pre.appendChild(document.createTextNode(message));
-        this.output.appendChild(pre);
+        const paragraph = document.createElement("p");
+        paragraph.appendChild(document.createTextNode(message));
+        this.output.appendChild(paragraph);
     }
 
     doSend(message) {
+        this.webSocket.send(message);
         this.writeToScreen("Message Sent: " + message);
-        this.websocket.send(message);
     }
 }
