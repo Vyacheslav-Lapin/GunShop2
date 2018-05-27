c36bb2c568596e1ff1ed76cd0d50bbd45415876b
Vyacheslav Lapin <Vyacheslav_Lapin@epam.com>
2018-05-28 01:27:24 +0300

    async style coding injected

--- servlets/src/main/webapp/_scripts/BaseServer.ts
+++ servlets/src/main/webapp/_scripts/BaseServer.ts
@@ -5,9 +5,9 @@ class BaseServer {
                 private readonly baseUrl: string = `/webapi/`) {
     }
 
-    protected getObject<T>(urlPart: string): Promise<T> {
-        return fetch(this.baseUrl + urlPart, { credentials: "same-origin" })
-            .then(response => response.json())
+    // const fetchAsync = async (url, props) => await (await fetch(url, props)).json();
+    protected async getObject<T>(urlPart: string): Promise<T> {
+        return (await fetch(this.baseUrl + urlPart, { credentials: "same-origin" })).json()
             .catch(status => Error(`JSON didn't load successfully; error code: ${status}`));
     }
 }
